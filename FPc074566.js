9ddd929fc7625eadf9d99324a85ace60a9814bcd
Gursharanpk <58919733+Gursharanpk@users.noreply.github.com>
2019-12-15 17:24:43 -0500

    Create FPc074566.js

---
+++ FPc074566.js
@@ -0,0 +1,24 @@
+/*
+Gursharan Preet Kaur
+C07477566
+*/
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Sukhpreet:Sunny987tt@cluster0-tyh4l.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true });
+console.log('Opening...');
+client.connect(err => {
+  const collection = client.db("TheBankofIndia").collection("c07477566");
+  console.log("Db connected");
+  var data = [
+                
+            ];
+  
+  collection.insertMany(data, function(err,res){
+    console.log("Data inserted");
+  });
+
+  
+    
+  client.close();
+  console.log('Db closed..');
+});
